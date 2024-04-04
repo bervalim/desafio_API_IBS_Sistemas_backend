3bb14cb2e426f3fe990b28aebba62ca7a45c6e9c
bervalim <bernardogvalim@gmail.com>
2024-04-04 20:50:51 -0300

    feat/adress-dto

--- src/modules/addresses/dto/create-address.dto.ts
+++ src/modules/addresses/dto/create-address.dto.ts
@@ -1,1 +1,38 @@
-export class CreateAddressDto {}
+import {
+  IsNotEmpty,
+  IsNumber,
+  IsString,
+  MaxLength,
+  MinLength,
+} from 'class-validator';
+
+export class CreateAddressDto {
+  @IsString()
+  @IsNotEmpty()
+  @MaxLength(8)
+  zipCode: string;
+  @IsString()
+  @IsNotEmpty()
+  @MaxLength(200)
+  address: string;
+  @IsNumber()
+  @IsNotEmpty()
+  number: number;
+  @IsString()
+  @IsNotEmpty()
+  @MaxLength(40)
+  complement: string;
+  @IsString()
+  @IsNotEmpty()
+  @MaxLength(40)
+  neighborhood: string;
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(2)
+  @MaxLength(2)
+  state: string;
+  @IsString()
+  @IsNotEmpty()
+  @MaxLength(50)
+  city: string;
+}
