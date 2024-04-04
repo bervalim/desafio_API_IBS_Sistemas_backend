76c629c6ba64e671f2375ccd4129eb7ef91441cc
bervalim <bernardogvalim@gmail.com>
2024-04-04 01:30:11 -0300

    feat/person-entity

--- src/modules/people/entities/person.entity.ts
+++ src/modules/people/entities/person.entity.ts
@@ -1,1 +1,19 @@
-export class Person {}
+import { Exclude } from 'class-transformer';
+import { randomUUID } from 'crypto';
+import { Sex, CivilState } from '@prisma/client';
+
+export class Person {
+  readonly id: string;
+  name: string;
+  email: string;
+  @Exclude()
+  password: string;
+  sex: Sex;
+  birthDate: string;
+  civilState: CivilState;
+  admin: boolean;
+
+  constructor() {
+    this.id = randomUUID();
+  }
+}
--- src/modules/people/people.module.ts
+++ src/modules/people/people.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { PeopleService } from './people.service';
 import { PeopleController } from './people.controller';
+import { PrismaService } from 'prisma/prisma.service';
 
 @Module({
   controllers: [PeopleController],
-  providers: [PeopleService],
+  providers: [PeopleService, PrismaService],
 })
 export class PeopleModule {}
