39e8aec81bec52be119d287cc21a0efb2c0635f8
bervalim <bernardogvalim@gmail.com>
2024-04-04 01:58:50 -0300

    feat/person-dto

--- src/modules/people/dto/create-person.dto.ts
+++ src/modules/people/dto/create-person.dto.ts
@@ -1,1 +1,44 @@
-export class CreatePersonDto {}
+import { Sex, CivilState } from '@prisma/client';
+import { hashSync } from 'bcrypt';
+import { Transform } from 'class-transformer';
+import {
+  IsBoolean,
+  IsEnum,
+  IsNotEmpty,
+  IsOptional,
+  IsString,
+  MaxLength,
+  MinLength,
+} from 'class-validator';
+
+export class CreatePersonDto {
+  @IsString()
+  @IsNotEmpty()
+  @MaxLength(80)
+  name: string;
+  @IsString()
+  @IsNotEmpty()
+  @MaxLength(45)
+  email: string;
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(8)
+  @MaxLength(80)
+  @Transform(({ value }: { value: string }) => hashSync(value, 10), {
+    groups: ['transform'],
+  })
+  password: string;
+  @IsNotEmpty()
+  @IsEnum(Sex)
+  sex: Sex;
+  @IsString()
+  @IsNotEmpty()
+  @MaxLength(80)
+  birthDate: string;
+  @IsNotEmpty()
+  @IsEnum(CivilState)
+  civilState: CivilState;
+  @IsBoolean()
+  @IsOptional()
+  admin: boolean;
+}
