4206c66f9697ff447a41b3a0335572e87cae824f
bervalim <bernardogvalim@gmail.com>
2024-04-10 16:47:08 -0300

    feat/authorization-finished

--- src/main.ts
+++ src/main.ts
@@ -21,11 +21,7 @@ async function bootstrap() {
     .addBearerAuth()
     .build();
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup(
-    'https://desafio-api-ibs-sistemas-backend-1.onrender.com/' || 'api',
-    app,
-    document,
-  );
+  SwaggerModule.setup('api', app, document);
   await app.listen(3000);
 }
 bootstrap();
