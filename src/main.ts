87acd06ae0ac1b6205d987a0e0c209b2148315c9
bervalim <bernardogvalim@gmail.com>
2024-04-15 17:54:36 -0300

    feat/inserting vercel link on cors

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,10 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'http://localhost:4200',
+    origin: [
+      'http://localhost:4200',
+      'https://ibs-sistemas-frontend.vercel.app',
+    ],
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true,
